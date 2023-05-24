d660bdbe4761f66e4acb207e084780de0c69c4fc
xenjamo <xenjamo@gmail.com>
2023-05-24 19:27:50 +0200

    decoding more relevant information, added mag calibration

--- DATA/20230522/bin_reader.m
+++ DATA/20230522/bin_reader.m
@@ -1,4 +1,6 @@
-fid = fopen('acc_calib.bin');
+clear all, clc, close all
+%% read file
+fid = fopen('003.bin');
 M = []; % initalize array
 A = [];
 A2 = [];
@@ -26,83 +28,177 @@ end
 
 fclose(fid);                        % close file
 
-%%
-close all;
+%% extract relevant data
 size(M)
-B(end,2)
+
+n = B(:,2);
 t = B(:,1)/1000000;
 dt = [0;diff(t)];
+dn = [0;diff(n)];
 t = t-t(1);
+itow = B(:,3);
+ditow = [0;diff(itow)];
+ttff = B(1,4);
+msss = B(:,5);
+dmsss = [0;diff(msss)];
+rtk_flags = B(:,6);
+
+gnss_fix = C(:,1);
+fix_type = C(:,2);
+numSV = C(:,3); % number of visible satellites
+lastCorrectionAge = C(:,4);
+
+
+gyro = A(:,1:3);
+accel = A(:,4:6);
+mag = A(:,7:9);
+A_mag = [0.9858873,  0.0000000,  0.0000000;
+         0.0105822,  0.9857083,  0.0000000;
+         0.0572703,  0.0053311,  1.0284045];
+b_mag = [0.4574184, -0.3294953,  0.3824048]';
+mag_calib = (mag - b_mag.')*A_mag.';
+
+
+quat = A(:,10:13);
+rpy = A(:,14:16);
+
+posCovNN = A(:,18);
+posCovNE = A(:,19);
+posCovND = A(:,20);
+posCovEE = A(:,21);
+posCovED = A(:,22);
+posCovDD = A(:,23);
+velCovNN = A(:,24);
+velCovNE = A(:,25);
+velCovND = A(:,26);
+velCovEE = A(:,27);
+velCovED = A(:,28);
+velCovDD = A(:,29);
+
+K_pos = [posCovNN, posCovNE, posCovND, posCovEE, posCovED, posCovDD];
+
+K_vel = [velCovNN, velCovNE, velCovND, velCovEE, velCovED, velCovDD];
+
+DOP = A(:,30:36);
+
+relposNEU = [A(:,37), A(:,38), -A(:,39)];
+relaccNEU = [A(:,40), A(:,41), A(:,42)];
+
+hMSL = A(:,43);
+hAcc = A(:,44);
+vAcc = A(:,45);
+
+llh = [A(:,46), A(:,47), A(:,48)];
+relvelNEU = [A(:,49), A(:,50), -A(:,51)];
+
+sAcc = A(:,52);
+gSpeed = A(:,53);
+heading = A(:,54)*pi/180;
+
+
+
+
+%% plots
+
 figure(999);
-plot(t,dt);
-title("perf")
+subplot(411)
+plot(t,gnss_fix);
+title("GPS Fix")
+grid on;
+subplot(412)
+plot(t,fix_type);
+title("Fix Type")
+grid on;
+subplot(413)
+plot(t,numSV);
+title("number of visible satellites")
+ylim([0 max(numSV)+5])
+grid on;
+subplot(414)
+plot(t,lastCorrectionAge);
+title("Last RTK Correction Age")
 grid on;
 
+figure(9999);
+subplot(411)
+plot(t,dt);
+title("dt Microcontroller")
+grid on;
+subplot(412)
+plot(t,dn);
+title("dSample")
+grid on;
+subplot(413)
+plot(t,ditow);
+title("dt GPS Time")
+grid on;
+subplot(414)
+plot(t,dmsss);
+title("dt RTC time")
+grid on;
+
+
 figure(1);
-plot(t,A(:,1));
-hold on;
-plot(t,A(:,2));
-plot(t,A(:,3));
-hold off;
+plot(t,gyro);
 title("gyro")
+grid on;
 
 figure(2);
-hold on;
-plot(t,A(:,6));
-hold off;
-title("acc")
+plot(t,accel);
+title("accel")
+grid on;
 
 figure(3);
-plot(t,A(:,7));
-hold on;
-plot(t,A(:,8));
-plot(t,A(:,9));
-hold off;
+subplot(211)
+plot(t,mag);
 title("mag")
+grid on;
+subplot(212);
+plot(t,mag_calib);
+title("mag calib")
+grid on;
+
+
 
 figure(5);
-plot(t,A(:,14));
-hold on;
-plot(t,A(:,15));
-plot(t,A(:,16));
-hold off;
+plot(t,rpy);
 title("rpy")
+grid on;
 
 
 figure(234);
-hold on;
-plot(t,A(:,30));
-plot(t,A(:,31));
-plot(t,A(:,32));
-plot(t,A(:,33));
-plot(t,A(:,34));
-plot(t,A(:,35));
-plot(t,A(:,36));
-hold off;
+plot(t,DOP);
 title("DOP")
+legend(["gDOP" "pDOP" "tDOP" "vDOP" "hDOP" "nDOP" "eDOP"], "location", "northwest")
+grid on;
 
 figure(2674);
-hold on;
-plot3(A(:,37),A(:,38),-A(:,39), '.');
-hold off;
+plot3(relposNEU(:,2),relposNEU(:,1),relposNEU(:,3), '.');
 title("RELPOS")
+grid on;
+
+figure(2675);
+plot3(relvelNEU(:,2),relvelNEU(:,1),relvelNEU(:,3), '.');
+title("RELVEL")
+grid on;
 
 figure(46);
-hold on;
-plot(t,A(:,40));
-plot(t,A(:,41));
-plot(t,A(:,42));
-hold off;
+plot(t,relaccNEU);
 title("RELacc")
 
+
 figure(84)
-hold on;
 plot3(A2(:,1), A2(:,2), A2(:,3), '.');
-hold off;
 title("hpposllh")
+
+figure(420)
+polarplot(heading,gSpeed,'.')
+title("Ground Speed")
+grid on;
 
 figure(456);
 plot(t,D-[0 2 4 6 8 10 12 14])
 ylim([-14 1])
-legend({"invaldLLH" "RTK Fix" "RTK float" "diff available" "velCov valid" "posCov valid" "svin valid" "time mode"}, "location", "southeast")
+legend(["invaldLLH" "RTK Fix" "RTK float" "diff available" "velCov valid" "posCov valid" "svin valid" "time mode"], "location", "southeast")
 title("status")
+grid on;
